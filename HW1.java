06eabf01ca1541677f4272714dfd1fe5f41c8e4b
rosekao871128 <56061923+rosekao871128@users.noreply.github.com>
2020-03-12 02:44:16 +0800

    Add files via upload

---
+++ HW1.java
@@ -0,0 +1,64 @@
+public class HW1{
+
+    public static void main(String[]args){
+        Student student1 = new Student();
+        
+        student1.setName("weee");
+        System.out.println("Name\t:"+student1.getName());
+
+
+        student1.setGrade("three");
+        System.out.println("Grade\t:"+student1.getGrade());
+
+        student1.setId("108021111");
+        System.out.println("Id\t:"+student1.getId());
+
+        student1.setGender("male");
+        System.out.println("Gender\t:"+student1.getGender());
+
+        student1.setFaculty("Information\tEngineering");
+        System.out.println("Faculty\t:"+student1.getFaculty());
+
+        
+        
+        Student student2 = new Student();
+        
+        student2.setName("wee");
+        System.out.println("Name\t:"+student2.getName());
+
+
+        student2.setGrade("two");
+        System.out.println("Grade\t:"+student2.getGrade());
+
+        student2.setId("108021112");
+        System.out.println("Id\t:"+student2.getId());
+
+        student2.setGender("male");
+        System.out.println("Gender\t:"+student2.getGender());
+
+        student2.setFaculty("Information\tEngineering");
+        System.out.println("Faculty\t:"+student2.getFaculty());
+
+
+        Student student3 = new Student();
+        
+        student3.setName("we");
+        System.out.println("Name\t:"+student3.getName());
+
+
+        student3.setGrade("one");
+        System.out.println("Grade\t:"+student3.getGrade());
+
+        student3.setId("108021113");
+        System.out.println("Id\t:"+student3.getId());
+
+        student3.setGender("female");
+        System.out.println("Gender\t:"+student3.getGender());
+
+        student3.setFaculty("Information\tEngineering");
+        System.out.println("Faculty\t:"+student3.getFaculty());
+
+
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,44 @@
+public class Student {
+    
+    private String name , grade , id,gender,faculty;
+
+    public void setName(String setname){
+        this.name = setname;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setGrade(String setgrade){
+        this.grade = setgrade;
+    }
+
+    public String getGrade(){
+        return grade;
+    }
+
+    public void setId(String setid){
+        this.id = setid;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public void setGender(String setgender){
+        this.gender = setgender;
+    }
+
+    public String getGender(){
+        return gender;
+    }
+
+    public void setFaculty(String setfaculty){
+        this.faculty = setfaculty;
+    }
+
+    public String getFaculty(){
+        return faculty;
+    }
+}
